0808604f131a6f58c1203bc05bd5c3c17228009f
usharik <usharik@mail.ru>
2019-02-24 14:00:57 +0100

    Auth filter example

---
+++ lesson2-filter/src/main/java/ru/geekbrains/filter/LogoutServlet.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.filter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "secure/logout_serv")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+        resp.sendRedirect(req.getContextPath());
+    }
+}
